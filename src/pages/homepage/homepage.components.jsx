f11be7dd3cf6299c1e2278469655d17362177266
Anurag Chitnis <anuragchitnis86@gmail.com>
2020-04-01 23:48:22 +0530

    Created with react

---
+++ src/pages/homepage/homepage.components.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../App.css";
+import './homepage.styles.scss';
+import Directory from "../../components/directory/directory.components";
+const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+);
+
+export default HomePage;
