fa53d938450b707e2580ded8e6c684fba246d620
Anurag Chitnis <anuragchitnis86@gmail.com>
2020-04-02 16:04:51 +0530

    added styles to shopage and header component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import {Switch, Route} from 'react-router-dom';
 
 import HomePage from "./pages/homepage/homepage.components";
 import ShopPage from './pages/shopage/shop.components';
-
+import Header from './components/header/header.components';
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component= {ShopPage} />
