b17babf101261e6b4f17883a2ae9f58ff2892549
Anurag Chitnis <anuragchitnis86@gmail.com>
2020-04-16 01:39:31 +0530

    Added CartIcon component with fix in little issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.components";
 import ShopPage from './pages/shopage/shop.components';
 import Header from './components/header/header.components';
@@ -45,15 +45,19 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route exact path='/shop' component= {ShopPage} />
-          <Route exact path='/signin' component={SignInAndSignUp}/>
+          <Route exact path='/signin' component={() => this.props.currentUser ? (<Redirect to='' />): (<SignInAndSignUp />)}/>
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
