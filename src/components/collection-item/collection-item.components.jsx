54426d0bdce1d2e2c8c2fe64e9baf5a65210b222
Anurag Chitnis <anuragchitnis86@gmail.com>
2020-04-18 23:14:18 +0530

    Added styling to button and collection-item component

--- src/components/collection-item/collection-item.components.jsx
+++ src/components/collection-item/collection-item.components.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.components';
+
 const CollectionItem = ({id, name, price, imageUrl}) => (
     <div className="collection-item">
         <div className="image" style={{
@@ -9,6 +11,7 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
             <div className="name">{name}</div>
             <div className="price">{price}</div>
         </div>
+        <CustomButton inverted>Add to cart</CustomButton>
     </div>
 )  
 
