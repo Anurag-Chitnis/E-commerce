a5fcb9b4480ad79accf809b191d34f8ca2b53a5a
Anurag Chitnis <anuragchitnis86@gmail.com>
2020-04-19 23:29:02 +0530

    Added cart-items to cart-dropdown with redux implementation also tweeked cart-dropdown styles

--- src/components/collection-item/collection-item.components.jsx
+++ src/components/collection-item/collection-item.components.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import './collection-item.styles.scss';
 import CustomButton from '../custom-button/custom-button.components';
+import {connect} from 'react-redux';
+import {addItem} from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className="collection-item">
-        <div className="image" style={{
-            backgroundImage: `url(${imageUrl})`
-        }}></div>
-        <div className="collection-footer">
-            <div className="name">{name}</div>
-            <div className="price">{price}</div>
-        </div>
-        <CustomButton inverted>Add to cart</CustomButton>
-    </div>
-)  
+const CollectionItem = ({item, addItem}) => {
+    const {id, name, price, imageUrl} = item;
+    return(
+        <div key={id} className="collection-item">
+            <div className="image" style={{
+                backgroundImage: `url(${imageUrl})`
+            }}></div>
+            <div className="collection-footer">
+                <div className="name">{name}</div>
+                <div className="price">{price}</div>
+            </div>
+            <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
+        </div>  
+    );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
