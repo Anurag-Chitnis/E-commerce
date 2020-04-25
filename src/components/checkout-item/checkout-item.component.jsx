c1fc99402cffb010ef5cbe7249e2340fd12f293c
Anurag Chitnis <anuragchitnis86@gmail.com>
2020-04-25 13:25:56 +0530

    added feature to remove items by remove arrow or add item with add arrow and when quantity is 1 remove feature is triggered

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './checkout-item.styles.scss';
 import {connect} from 'react-redux';
-import {clearItemFromCart} from '../../redux/cart/cart.actions';
+import {clearItemFromCart, addItem, removeItem} from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({cartItem, clearItemFromCart}) => {
+const CheckoutItem = ({cartItem, clearItemFromCart, addItem, removeItem}) => {
     const {imageUrl, name, price, quantity} = cartItem;
     return(
         <div className="checkout-item">
@@ -12,9 +12,9 @@ const CheckoutItem = ({cartItem, clearItemFromCart}) => {
             </div>
             <span className='name'>{name}</span>
             <span className='quantity'>
-                <div className="arrow">&#10094;</div>
+                <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
                     <span className='value'>{quantity}</span>
-                <div className="arrow">&#10095;</div>   
+                <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>   
             </span>
             <span className='price'>{price}</span>
             <div className='remove' onClick={() => clearItemFromCart(cartItem)}>
@@ -25,7 +25,9 @@ const CheckoutItem = ({cartItem, clearItemFromCart}) => {
 }
 
 const mapDispatchToProps = dispatch => ({
-    clearItemFromCart: item => dispatch(clearItemFromCart(item))
+    clearItemFromCart: item => dispatch(clearItemFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item)) 
 })
 
  
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,6 +7,11 @@ export const addItem = item => ({
     payload: item
 })
 
+export const removeItem = item => ({
+    type: "REMOVE_ITEM",
+    payload: item
+})
+
 export const clearItemFromCart = item => ({
     type: "CLEAR_ITEM_FROM_CART",
     payload: item
