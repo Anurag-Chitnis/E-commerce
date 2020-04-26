19825b70114feb399fcdc2c9293f5eff7c3aefac
Anurag Chitnis <anuragchitnis86@gmail.com>
2020-04-26 23:39:51 +0530

    added the styles to collection-page component and home redirectts to specific catrgory as per parameter

--- src/pages/collection/collection.components.jsx
+++ src/pages/collection/collection.components.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import './collection.styles.scss';
 import {connect} from 'react-redux';
 import {selectCollection} from '../../redux/shop/shop.selector';
+import CollectionItem from '../../components/collection-item/collection-item.components';
 
-const CollectionPage = ({collection}) => {
-    console.log(collection);
+
+const CollectionPage = ({collection: {title, items}}) => {
+    console.log(items);
     return(
         <div className="collection-page">
-            <h2>Hello Boys</h2>
+            <div className="title">{title}</div>
+            <div className="items">
+                {
+                    items.map(item => (<CollectionItem key={item.id} item={item}/>))
+                }
+            </div>
         </div>
     )
 }
